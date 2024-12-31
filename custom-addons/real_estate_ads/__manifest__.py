d3a421759deb7080bbe9f14716814b82649f6ffc
MagaDev <magahr123@gmail.com>
2024-12-31 17:30:56 -0400

    31-12-2024 - Creating the new module

---
+++ custom-addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate Ads",
+    'version':"1.0",
+    'website':"https//www.estoesunaprueba.com",
+    'author':'Maga',
+    'description': """
+         Real Estate module to show available properties
+    """,
+    'category': 'Sales',
+   # any module necessary for this one to work correctly
+    'depends': [''],
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'instalable':True,
+    'application':True,
+    'license':'LGPL-3',
+    'demo': [
+        'demo/demo.xml',
+    ]
+}
+
